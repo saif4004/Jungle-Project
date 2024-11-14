69deea1a3ea58f74291e985704b682fc8b5f9a60
Saif Al-saedi <saif4004@gmail.com>
2024-11-13 16:38:50 -0800

    Admin Security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -25,6 +26,11 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
 
   def product_params
     params.require(:product).permit(
